6a962cfc177a2bd98dc1550a9625bab097cae676
RaselHaulader <hauladerrasel2@gmail.com>
2021-12-18 17:31:48 +0600

    added mongo

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
+
+app.use(express.json());
+app.use(cors());
 app.use(express.json());
 
-const cors = require('cors')
-app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.send('hello')
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.x89oq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('connected to db');
+    const propertyBazar = client.db("PropertyBazar");
+    const propertiesCollection = propertyBazar.collection('allProperties')
+    const usersCollection = propertyBazar.collection('users')
+
+
+    //post api
+    app.post('/post', async (req, res) => {
+      const properties = req.body;
+      const result = await propertiesCollection.insertOne(properties);
+      res.json(result)
+    })
+    // get 3 properties
+    app.get('/allProperties3', async (req, res) => {
+      const query = propertiesCollection.find({})
+      const result = await query.limit(3).toArray();
+      res.json(result)
+    })
+    // get 4 properties
+    app.get('/allProperties4', async (req, res) => {
+      const query = propertiesCollection.find({})
+      const result = await query.limit(4).toArray();
+      res.json(result)
+    })
+    app.get('/allProperties', async (req, res) => {
+      const query = propertiesCollection.find({})
+      const result = await query.toArray();
+      res.json(result)
+    })
+
+
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('hello')
 })
 
 
