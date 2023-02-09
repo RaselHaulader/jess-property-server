4f610a0634d5cfcf6c515b429ac88466396308c4
Rasel <hauladerrasel2@gmail.com>
2023-02-10 02:09:40 +0600

    updated json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
